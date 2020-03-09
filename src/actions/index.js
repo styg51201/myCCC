0deedbaf1f19b522a3f3d4b07ca9fed3d494b747
Eason Liu <onpica17@gmail.com>
2020-03-09 17:48:42 +0800

    add actions and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// action = {type, value}
+// type: ADD_VALUE, MINUS_VALUE
+// ex. action = {type: 'ADD_VALUE', value: 10}
+
+// export const addValue = value => ({ type: 'ADD_VALUE', value: value })
+// export const minusValue = value => ({ type: 'MINUS_VALUE', value: value })
+
+// export const addValueAsync = value => {
+//   return dispatch => {
+//     setTimeout(() => {
+//       console.log('delay 3s to add value')
+//       dispatch(addValue(value))
+//     }, 3000)
+//   }
+// }
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux'
+
+// 第一步：建立reducer
+// action = {type, value}
+// type: ADD_VALUE, MINUS_VALUE
+// ex. action = {type: 'ADD_VALUE', value: 10}
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'ADD_VALUE':
+      return state + action.value
+    case 'MINUS_VALUE':
+      return state - action.value
+    default:
+      return state
+  }
+}
+
+// 合併多個reducer (必要，為了要配合瀏覽器開發外掛使用)
+const rootReducer = combineReducers({
+  counter,
+})
+
+export { rootReducer }
