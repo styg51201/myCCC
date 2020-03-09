6668b532b53f21dcfe0a206bcb9c349f94db14ac
nichaschang <nichaschang@gmail.com>
2020-03-10 02:06:48 +0800

    新增購物車右側總計版面

---
+++ src/components/MaoCartShopTotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function MaoCartShopTotal(){
+    return(
+        <>    
+           <div className="border bg-white" style={{height:"415px",width:"300px",marginLeft:"10px"}}>
+           </div>
+        </>
+    )
+}
+
+export default MaoCartShopTotal
