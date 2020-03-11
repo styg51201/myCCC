cd9b5b26851c1230a84dc1bffdeac66da92932b7
stacey <styg51201@gmail.com>
2020-03-11 18:46:38 +0800

    add redux 起手式

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// 導入所需模組與方法
+import { Provider } from 'react-redux'
+
+import { createStore, applyMiddleware, compose } from 'redux'
+
+import thunk from 'redux-thunk'
+
+import { rootReducer } from './reducers/index'
+
+// ---- 使用 Middleware時 創建store ----
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
