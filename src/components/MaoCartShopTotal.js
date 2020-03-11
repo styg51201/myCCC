576103f69a2d4f8d0c35e136ad9e8b5a65198539
Chang <morweng0113@gmail.com>
2020-03-11 10:01:23 +0800

    切版內容新增總計欄位

--- src/components/MaoCartShopTotal.js
+++ src/components/MaoCartShopTotal.js
@@ -1,13 +1,105 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+function MaoCartShopTotal() {
+  useEffect(() => {
+    let TotalBox = document.querySelector('.Total-box')
 
+    document.addEventListener('scroll', e => {
+      let FixScrollY = e.path[1].scrollY
+      let tt = e.path[1].pageXOffset
 
-function MaoCartShopTotal(){
-    return(
-        <>    
-           <div className="border bg-white" style={{height:"415px",width:"300px",marginLeft:"10px"}}>
-           </div>
-        </>
-    )
+      // console.log(TotalBox)
+      if (FixScrollY > 170) {
+        TotalBox.classList.add('position-fixed')
+        console.log(FixScrollY)
+      } else {
+        TotalBox.classList.remove('position-fixed')
+      }
+
+      //  console.log(e.path[1].scrollY)
+    })
+  }, [])
+  return (
+    <>
+      <div
+        className="border bg-white Total-box"
+        style={{
+          height: '435px',
+          width: '300px',
+          marginLeft: '10px',
+          padding: '10px',
+          top: '90px',
+          right: '317px',
+        }}
+      >
+        <p style={{ borderBottom: '1px solid #ddd', padding: '10px 0px' }}>
+          <b>TOTAL</b>
+        </p>
+        <div style={{ fontSize: '15px' }}>
+          <div style={{ marginBottom: '16px' }}>
+            <b>運費</b>
+            <span>100</span>
+          </div>
+          <div style={{ marginBottom: '16px' }}>
+            <b>小計金額</b>
+          </div>
+          <div style={{ marginBottom: '16px' }}>
+            <b>活動折扣</b>
+          </div>
+          <label htmlFor="coupon">
+            <div style={{ marginBottom: '16px' }}>
+              <b>折價券輸入</b>
+              <input
+                type="text"
+                placeholder="輸入折扣碼"
+                id="coupon"
+                style={{
+                  width: '280px',
+                  border: 'none',
+                  borderBottom: '1px solid #eee',
+                  outline: 'none',
+                }}
+              />
+            </div>
+          </label>
+        </div>
+        <p style={{ fontSize: '18px' }}>
+          <b>總金額</b>
+        </p>
+        <Link
+          to=""
+          style={{
+            width: '270px',
+            height: '45px',
+            border: '1px solid #000',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '15px 0px',
+            color: '#000',
+          }}
+        >
+          繼續購物
+        </Link>
+        <Link
+          to=""
+          style={{
+            width: '270px',
+            height: '45px',
+            border: '1px solid #000',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '15px 0px',
+            color: '#fff',
+            background: '#000',
+          }}
+        >
+          前往結帳
+        </Link>
+      </div>
+    </>
+  )
 }
 
-export default MaoCartShopTotal
+export default withRouter(MaoCartShopTotal)
--- src/mao/ShopCartList.js
+++ src/mao/ShopCartList.js
@@ -1,86 +1,189 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 // import logo from '../logo.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button } from 'react-bootstrap'
 import Toast from 'react-bootstrap/Toast'
-import { withRouter,Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import MaoCartShopTotal from '../components/MaoCartShopTotal'
 
-function CartList(props){
-    const ExampleToast = ({ children }) => {
-        const [show, toggleShow] = useState(false)
-    
-        return (
-          <>
-            {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-            <Toast show={show} onClose={() => toggleShow(false)}>
-              <Toast.Header>
-                <strong className="mr-auto">歡迎光臨</strong>
-              </Toast.Header>
-              <Toast.Body>{children}</Toast.Body>
-            </Toast>
-          </>
-        )
-      }
-      const dataList=[]
-      for(let i=0;i<5;i++){
-        dataList.push(
-            (
-                <li className="d-flex" style={{padding:"15px 0px",width:"900px",margin:"0px auto",borderBottom:"2px solid #efefef"}}>
-                <img src="https://fakeimg.pl/125/" style={{margin:"0px 5px"}}/>
-                <div className="d-flex flex-column justify-content-between" style={{margin:"0px 50px"}}>
-                    <p style={{fontSize:"18px"}}><b>SONY 真無線降噪入耳式耳機WF-1000XM3</b></p>
-                    <div className="d-flex justify-content-between">
-                        <p style={{fontSize:"18px"}}>價格7000</p>
-                           <div className="d-flex justify-content-between align-items-center" style={{height:"30px",overflow:"hidden",border:"1px solid #ddd",width:"85px"}}>
-                           <div className="text-center" style={{fontSize:"30px",background:"#ddd",width:"30%"}}>-</div>
-                               <input value="1" type="text" className="text-center w-50 m-0" style={{outline:"none",border:"none"}}/>
-                               <div className="text-center" style={{fontSize:"30px",background:"#ddd",width:"30%"}}>+</div>
-                           </div>
-                        <p style={{fontSize:"18px"}}><b>$7000</b></p>
-                    </div>
-                </div>
-                <div className="d-flex flex-column justify-content-center text-left" style={{margin:"auto"}}>
-                    <div className="border d-flex align-items-center"  style={{padding:"8px 20px",margin:"10px"}} >
-                       <img src="./img/header-footer/shopping-bag.svg" style={{marginRight:"10px"}} alt=""/>
-                       <span>刪除</span>
-                    </div>
-                    <div className="border d-flex align-items-center" style={{padding:"8px 20px",margin:"10px"}} >
-                       <img src="./img/header-footer/heart.svg" style={{marginRight:"10px"}} alt=""/>
-                       <span>下次購買</span>
-                    </div>
-                </div>
-            </li>
-          )
-        )
-      }
-      
-    const behavior=(
-        <ul className="nav nav-tabs" id="myTab" role="tablist">
-    <li className="nav-item">
-      <Link className="nav-link active" id="home-tab" data-toggle="tab" to="/CartList" role="tab" aria-controls="home" aria-selected="true">購物車</Link>
-    </li>
-    <li className="nav-item">
-      <Link className="nav-link" id="profile-tab" data-toggle="tab" to="/CartList" role="tab" aria-controls="profile" aria-selected="false">收藏</Link>
-    </li>
-  </ul>
-      )
-    return(
-        <>  
-        <div className="w-100" style={{background:"#efefef",padding:"100px"}}>
-        
-         <div style={{width:"1300px",margin:"0 auto"}}>
-         {behavior}
-            <div className="d-flex" >
-             <ul className="list-unstyled bg-white" style={{width:"975px",marginLeft:"1px"}}>
-                {dataList}
-             </ul>
-             <MaoCartShopTotal/>
-         </div>
-         </div>
-         </div>
-        </>
+function CartList() {
+  const [count, setCount] = useState(1)
+  const dataList = []
+  const data = [1, 2, 5, 9]
+
+  function a(e) {
+    let getNum = +e.target.getAttribute('data-value') * 1
+    let b = e.target.parentNode.children[1]
+    b.value = +b.value + getNum
+  }
+
+  const handleCount = e => {
+    console.log(e)
+  }
+  for (let i = 0; i < data.length; i++) {
+    dataList.push(
+      <li
+        key={data[i]}
+        className="d-flex"
+        style={{
+          padding: '15px 0px',
+          width: '900px',
+          margin: '0px 15px',
+          borderBottom: '2px solid #efefef',
+          padding: '10px',
+        }}
+      >
+        <img src="https://fakeimg.pl/125/" style={{ margin: '0px 20px' }} />
+        <div
+          className="d-flex flex-column justify-content-between"
+          style={{ margin: '0px 50px' }}
+        >
+          <p style={{ fontSize: '18px' }}>
+            <b>SONY 真無線降噪入耳式耳機WF-1000XM3</b>
+          </p>
+          <div className="d-flex justify-content-between">
+            <p style={{ fontSize: '18px' }}>價格7000</p>
+            <div
+              className="d-flex justify-content-between align-items-center"
+              style={{
+                height: '30px',
+                overflow: 'hidden',
+                border: '1px solid #ddd',
+                width: '85px',
+              }}
+            >
+              <button
+                className="text-center"
+                style={{ fontSize: '30px', background: '#ddd', width: '30%' }}
+                data-value="-1"
+                onClick={(e) => handleCount(e)}
+              >
+                -
+              </button>
+              <input
+                placeholder="1"
+                value={count}
+                type="text"
+                className="text-center w-50 m-0"
+                style={{ outline: 'none', border: 'none' }}
+              />
+              <button
+                className="text-center"
+                style={{ fontSize: '30px', background: '#ddd', width: '30%' }}
+                data-value="1"
+                onClick={a}
+              >
+                +
+              </button>
+            </div>
+            <p style={{ fontSize: '18px' }}>
+              <b>$7000</b>
+            </p>
+          </div>
+        </div>
+        <div
+          className="d-flex flex-column justify-content-center text-left"
+          style={{ margin: '0 auto' }}
+        >
+          <div
+            className="border d-flex align-items-center"
+            style={{ padding: '8px 20px', margin: '10px' }}
+          >
+            <img
+              src="..\img\header-footer\heart.svg"
+              style={{ marginRight: '10px' }}
+              alt=""
+            />
+            <span>刪除</span>
+          </div>
+          <div
+            className="border d-flex align-items-center"
+            style={{ padding: '8px 20px', margin: '10px' }}
+          >
+            <img
+              src="..\img\header-footer\search.svg"
+              style={{ marginRight: '10px' }}
+              alt=""
+            />
+            <span>下次購買</span>
+          </div>
+        </div>
+      </li>
     )
+  }
+
+  const behavior = (
+    <ul
+      className="nav nav-tabs"
+      id="myTab"
+      role="tablist"
+      style={{ width: '975px' }}
+    >
+      <li className="nav-item">
+        <Link
+          className="nav-link active"
+          id="home-tab"
+          data-toggle="tab"
+          to="/CartList"
+          role="tab"
+          aria-controls="home"
+          aria-selected="true"
+        >
+          購物車
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          id="profile-tab"
+          data-toggle="tab"
+          to="/CartList"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >
+          收藏
+        </Link>
+      </li>
+    </ul>
+  )
+  return (
+    <>
+      {behavior}
+      <div
+        className="d-flex justify-content-between"
+        style={{ position: 'relative' }}
+      >
+        <ul
+          className="list-unstyled bg-white"
+          style={{ maxWidth: '975px', marginLeft: '1px' }}
+        >
+          {dataList}
+          <li>
+            <Link
+              style={{
+                border: '1px solid #000',
+                maxWidth: '315px',
+                margin: '30px auto',
+                display: 'block',
+                textAlign: 'center',
+                padding: '10px',
+                color: '#fff',
+                background: '#000',
+              }}
+              to="/"
+            >
+              前往結帳
+            </Link>
+          </li>
+        </ul>
+        <MaoCartShopTotal />
+      </div>
+      {/* </div> */}
+
+      {/* </div> */}
+    </>
+  )
 }
 
 export default withRouter(CartList)
