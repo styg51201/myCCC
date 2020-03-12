05418b44ddc73ecad423bbea505866d7488dab93
stacey <styg51201@gmail.com>
2020-03-12 13:00:35 +0800

    優惠券專區

--- src/stacey/components/CouponItem.js
+++ src/stacey/components/CouponItem.js
@@ -12,12 +12,34 @@ import {getCoupon} from '../../actions/index'
 
 function CouponItem(props){
 
+  // const [couponState,setCouponState] = useState('領取')
+  
+  // 設定按鈕裡的字樣
+  let couponState = '領取'
+  if(props.data.getedCount === props.data.count){
+    couponState = '發放結束'
+  }else if(props.data.geted){
+    couponState = '去逛逛'
+  }
 
-  let couponClassName = classNames('col','col-sm-6','coupon',{
+  //設定優惠券的外觀
+  let couponClassName = classNames('col','col-sm-6','sty-coupon',{
     geted:props.data.geted,
+    end:props.data.getedCount === props.data.count,
   })
 
-  let count = {background: `linear-gradient(to right, #0dd2c5 ${props.data.count}%, #a0a0a0 ${props.data.count}%)`}
+  //設定進度條的外觀
+  let getedCountStyle = {background: `linear-gradient(to right, #0dd2c5 ${props.data.getedCount}%, #a0a0a0 ${props.data.getedCount}%)`}
+  let endCouponStyle = { background:'#a0a0a0'}
+
+  // 設定按鈕種類
+  let getButton = (<button disabled={couponState === '發放結束'}
+                        onClick={()=>{props.getCoupon(props.data) }}>
+                        <span>{couponState}</span>
+                     </button>)
+  let shopButton = (<button onClick={()=>{}}>
+                      <span>{couponState}</span>
+                    </button>)
 
   console.log('props',props)
     
@@ -37,14 +59,12 @@ function CouponItem(props){
                     <li>有效至 {props.data.dueDate}</li>
                   </ul>
                   <div className="state">
-                    <div style={count}></div>
-                    <p>{props.data.count}% 已領取</p>
+                    <div style={couponState === '發放結束'?endCouponStyle:getedCountStyle}></div>
+                    {couponState === '發放結束'?<p>全數領取完畢</p>:<p>{props.data.getedCount}% 已領取</p>}
                   </div>
                 </div>
                 <div className="button">
-                  <button onClick={()=>{props.getCoupon(props.data.id,!props.data.geted)}}>
-                    <span>領取</span>
-                  </button>
+                  {props.data.geted? shopButton:getButton}
                 </div>
               </div>
             </div>
