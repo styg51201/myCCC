caa736c056bf913b12d772e12039e2967be428f4
Biki <nightfallvs0923@gmail.com>
2020-03-13 11:09:49 +0800

    update node server & connect_db

--- src/db/server.js
+++ src/db/server.js
@@ -1,15 +1,28 @@
 const express = require('express') //express
 const session = require('express-session') //session
 const cors = require('cors') //CORS
+const db = require('./db_connect')
 
 //建立webserver物件
 const app = express();
+
+
+//test db
+app.get('/try-db', (req, res)=>{
+    let sql='SELECT * FROM `user_comment` '
+    db.queryAsync(sql)
+    .then(r=>{
+        return res.json(r)
+    })
+})
 
 //test
 app.get('/', (req, res)=>{
     res.send("hello")
 })
 
+
+//404
 app.use((req, res)=>{
     res.send("404 not found")
 })
