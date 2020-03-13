f75d7f5610cf33b5fd8ca02b6ef5248250c2758c
Biki <nightfallvs0923@gmail.com>
2020-03-13 11:13:04 +0800

    update Node server session & cors

--- src/db/server.js
+++ src/db/server.js
@@ -6,6 +6,33 @@ const db = require('./db_connect')
 //建立webserver物件
 const app = express();
 
+app.use(session({
+    saveUninitialized : false,
+    resave: false,
+    secret: 'whatever123456',
+    cookie:{
+        masAge: 1200000
+    }
+}));
+
+//CORS
+//記得前端設定credentials = 'include'
+const whiteList = [
+    'http://localhost:3000',
+    undefined
+]
+const corsOptions = {
+    credentials: true,
+    origin: function(origin, callback){
+        if(whiteList.indexOf(origin) !== -1){
+            callback(null, true)
+        }else{
+            callback(null, false)
+        }
+    }
+}
+app.use(cors(corsOptions));
+
 
 //test db
 app.get('/try-db', (req, res)=>{
