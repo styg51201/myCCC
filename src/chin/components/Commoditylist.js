8b7f5bfc19799c94cb77ff51011c41fe2bb2fc99
Hello <you257963148@gmail.com>
2020-03-15 22:08:00 +0800

    sort

--- src/chin/components/Commoditylist.js
+++ src/chin/components/Commoditylist.js
@@ -2,24 +2,48 @@ import React from 'react'
 
 function Commoditylist(){
     return(
-        <div className="chin-commoditylist">
-            <div className="chin-price">
-                <span>價格</span>
-                <img src="./chin-img/chevron-up.svg" alt=""/>
-            </div>
-            <div className="chin-price">
-                <span>品牌</span>
-                <img src="./chin-img/chevron-up.svg" alt=""/>
-            </div>
-            <div className="chin-price">
-                <span>功能</span>
-                <img src="./chin-img/chevron-up.svg" alt=""/>
-            </div>
-            <div className="chin-price">
-                <span>優惠</span>
-                <img src="./chin-img/chevron-up.svg" alt=""/>
-            </div>
-        </div>
+        <ul className="chin-commoditylist">
+            <li>
+                <div className="chin-price">
+                    <span>價格</span>
+                    <img src="./chin-img/chevron-up.svg" alt=""/>
+                </div>
+            </li>
+            <li>
+                <div className="chin-price">
+                    <span>品牌</span>
+                    <img src="./chin-img/chevron-up.svg" alt=""/>
+                </div>
+                <div>
+                    <ul>
+                        <li><input type="checkbox" />Apple</li>
+                        <li><input type="checkbox" />Samsung</li>
+                        <li><input type="checkbox" />Audio-Technica</li>
+                        <li><input type="checkbox" />Audio-Technica2</li>
+                        <li><input type="checkbox" />Audio-Technica3</li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div className="chin-price">
+                    <span>功能</span>
+                    <img src="./chin-img/chevron-up.svg" alt=""/>
+                </div>
+                <div>
+                    <ul>
+                        <li><input type="checkbox" />GPS 定位</li>
+                        <li><input type="checkbox" />運動偵測</li>
+                        <li><input type="checkbox" />藍芽</li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div className="chin-price">
+                    <span>優惠</span>
+                    <img src="./chin-img/chevron-up.svg" alt=""/>
+                </div>
+            </li>
+        </ul>
          )
 }
 
