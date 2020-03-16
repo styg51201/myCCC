11bf73466995ac07d2d4f7c73bd6fbc6d026fdf6
nichaschang <nichaschang@gmail.com>
2020-03-17 02:19:43 +0800

    更新root

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import  {getCouponData ,filterCoupon} from '../stacey/reducer/couponReducer'
 import { getItems,getItemId } from '../chin/reducer/itemsReducer'
+import { AddItem,displayShopCart,getShop } from '../mao/reducers/ShopCartReducer'
 
 //老師範例
 // 第一步：建立reducer
@@ -22,7 +23,7 @@ import { getItems,getItemId } from '../chin/reducer/itemsReducer'
 
 // 合併多個reducer (必要，為了要配合瀏覽器開發外掛使用)
 const rootReducer = combineReducers({
- getCouponData,filterCoupon,getItems,getItemId
+ getCouponData,filterCoupon,getItems,getItemId,AddItem,displayShopCart,getShop
 })
 
 export { rootReducer }
