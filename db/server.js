4d2d1070a52c1065870925c390e96fdbeeaad957
Chang <morweng0113@gmail.com>
2020-03-18 14:55:10 +0800

    新增購物車連結

--- db/server.js
+++ db/server.js
@@ -2,73 +2,69 @@ const express = require('express') //express
 const session = require('express-session') //session
 const cors = require('cors') //CORS
 const db = require('./db_connect')
-const bodyParser = require('body-parser');//解析POST參數
+const bodyParser = require('body-parser') //解析POST參數
 
 //建立webserver物件
-const app = express();
+const app = express()
 
-app.use(session({
-    saveUninitialized : false,
+app.use(
+  session({
+    saveUninitialized: false,
     resave: false,
     secret: 'whatever123456',
-    cookie:{
-        masAge: 1200000
-    }
-}));
+    cookie: {
+      masAge: 1200000,
+    },
+  })
+)
 
 //CORS
 //記得前端設定credentials = 'include'
-const whiteList = [
-    'http://localhost:3000',
-    undefined
-]
+const whiteList = ['http://localhost:3000', undefined]
 const corsOptions = {
-    credentials: true,
-    origin: function(origin, callback){
-        if(whiteList.indexOf(origin) !== -1){
-            callback(null, true)
-        }else{
-            callback(null, false)
-        }
+  credentials: true,
+  origin: function(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(null, false)
     }
+  },
 }
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-// post 傳送格式有兩種 urlencoded 和 json 
-app.use(bodyParser.urlencoded({ extended: false })); //解析urlencoded格式 
-app.use(bodyParser.json()); //解析json格式
-
+// post 傳送格式有兩種 urlencoded 和 json
+app.use(bodyParser.urlencoded({ extended: false })) //解析urlencoded格式
+app.use(bodyParser.json()) //解析json格式
 
 //test db
-app.get('/try-db', (req, res)=>{
-    let sql='SELECT * FROM `user_comment` '
-    db.queryAsync(sql)
-    .then(r=>{
-        return res.json(r)
-    })
+app.get('/try-db', (req, res) => {
+  let sql = 'SELECT * FROM `user_comment` '
+  db.queryAsync(sql).then(r => {
+    return res.json(r)
+  })
 })
 
 //test items
-app.use('/items', require(__dirname + '/items'));
-
+app.use('/items', require(__dirname + '/items'))
+//stories
+app.use('/shopCart', require(__dirname + '/shopCart'))
 //coupon
-app.use('/getCoupon', require(__dirname + '/getCoupon'));
+app.use('/getCoupon', require(__dirname + '/getCoupon'))
 
 //stories
-app.use('/stories', require(__dirname + '/stories'));
+app.use('/stories', require(__dirname + '/stories'))
 
 //test
-app.get('/', (req, res)=>{
-    res.send("hello")
+app.get('/', (req, res) => {
+  res.send('hello')
 })
-
 
 //404
-app.use((req, res)=>{
-    res.send("404 not found")
+app.use((req, res) => {
+  res.send('404 not found')
 })
 
-
-app.listen(5500, ()=>{
-    console.log('Express server start')
+app.listen(5500, () => {
+  console.log('Express server start')
 })
