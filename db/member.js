2055d2e54ef9d6d59b371a78f6ca41d8a455a37f
IreneLiu0421 <hollywooodliu@gmail.com>
2020-03-19 21:07:03 +0800

    調整css與新增會員登入部分

--- db/member.js
+++ db/member.js
@@ -18,4 +18,13 @@ router.get('/:memberaccount?', (req, res) => {
   })
 })
 
+//抓會員的account資料轉json貼入資料編輯畫面
+router.get('/edit/:memberaccount?', (req, res) => {
+  // res.json(req.params)
+  let sql = 'SELECT * FROM `member` WHERE `Account` = ?'
+  db.queryAsync(sql, [req.params.memberaccount]).then(r => {
+    return res.json(r)
+  })
+})
+
 module.exports = router
