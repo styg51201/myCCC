279b7af467c75d8f5f6f7caba118611390a7c116
IreneLiu0421 <hollywooodliu@gmail.com>
2020-03-20 15:55:07 +0800

    會員註冊v0

---
+++ src/Irene/components/MemberLoginRight.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import '../I_css/MemberLogin.scss'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { userRegisterAsync } from '../actions/memberAction'
+
+function MemberLoginRight(props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [error, setError] = useState(false)
+  const [errorMessages, setErrorMessages] = useState([])
+
+  const handleSubmit = () => {
+    let error = false
+    let errorMessages = []
+
+    if (!username) {
+      // console.log('nousername')
+      error = true
+      errorMessages.push('請輸入帳號')
+    }
+
+    if (!password) {
+      error = true
+      errorMessages.push('請輸入密碼')
+    }
+
+    if (!email) {
+      error = true
+      errorMessages.push('請輸入電子郵件')
+    }
+
+    if (error) {
+      setError(error)
+      setErrorMessages(errorMessages)
+      return
+    }
+
+    const userData = { username, password, email }
+    // props.userRegisterAsync(userData, () => alert('成功登入'))
+    props.userRegisterAsync(userData, () => console.log('前端傳資料成功'))
+  }
+
+  return (
+    <>
+      {error ? (
+        <>
+          <div className="alert alert-danger" role="alert">
+            {errorMessages.map((v, i) => (
+              <p key={i}>{v}</p>
+            ))}
+          </div>
+        </>
+      ) : (
+        ''
+      )}
+      <div className="row my-2 memberlogin">
+        <div className="col card">
+          <h3 className="MemberLoginh3">Register Now 快速註冊</h3>
+          <form>
+            <div className="row my-2">
+              <label className="col-3">帳號：</label>
+              <input
+                name="username"
+                className="form-control col-6"
+                type="text"
+                onChange={e => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="row my-2">
+              <label className="col-3">密碼：</label>
+
+              <input
+                name="password"
+                className="form-control  col-6"
+                type="text"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="row my-2">
+              <label className="col-3">電子信箱：</label>
+              <input
+                name="email"
+                className="form-control col-6"
+                type="text"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="irene-checkbox">
+              <input type="checkbox" />
+              <span>我同意會員服務條款與隱私權聲明</span>
+            </div>
+            <button
+              type="button"
+              className="irene-register-button"
+              style={{ margin: '20px auto', display: 'block' }}
+              onClick={() => handleSubmit()}
+            >
+              REGISTER NOW
+            </button>
+          </form>
+
+          {/* //如果可以要做google&facebook登入*/}
+        </div>
+      </div>
+    </>
+  )
+}
+
+// 取得Redux中isAuth的值
+const mapStateToProps = store => {
+  return { isAuth: store.member }
+}
+
+// 指示dispatch要綁定哪些action creators
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ userRegisterAsync }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemberLoginRight)
--- src/chin/components/CompareProductSort.js
+++ src/chin/components/CompareProductSort.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 //classnames
 import classNames from 'classnames'
@@ -7,71 +7,116 @@ import classNames from 'classnames'
 import { connect } from 'react-redux'
 //action
 import { bindActionCreators } from 'redux'
-import {PriceHightToLow,PriceLowToHight,NewTimeSort,HotItemsSort,AllItemsSort} from '../actions/itemsActions'
+import {
+  PriceHightToLow,
+  PriceLowToHight,
+  NewTimeSort,
+  HotItemsSort,
+  AllItemsSort,
+} from '../actions/itemsActions'
 
-function CompareProductSort(props){
-    const [featuredproducts,setFeaturedproducts]=useState(false)
-    const [sortname,setSortname]=useState('排序方式')
-    const SortClassName= classNames('chin-sort-featuredproducts',{active:featuredproducts})
+function CompareProductSort(props) {
+  const [featuredproducts, setFeaturedproducts] = useState(false)
+  const [sortname, setSortname] = useState('排序方式')
+  const SortClassName = classNames('chin-sort-featuredproducts', {
+    active: featuredproducts,
+  })
 
-    return(
+  return (
     <div className="chin-title">
-        <div className="chin-title-text">
-            <span>{props.englishname}</span>
-            <span>{props.data[0] ? props.data[0].itemCategoryId : ''}</span>
+      <div className="chin-title-text">
+        <span>{props.englishname}</span>
+        <span>{props.data[0] ? props.data[0].itemCategoryId : ''}</span>
+      </div>
+      <div className="chin-rwd-sort-features">
+        <button className="chin-rwd-features">功能</button>
+        <button className="chin-rwd-sort">排序方式</button>
+      </div>
+      <div className="chin-comparegoods-sort">
+        <button className="chin-comparegoods">
+          <span>比較商品</span>
+          <img src="./chin-img/align-justify.svg" alt="" />
+        </button>
+        <div className={SortClassName}>
+          <label
+            className="chin-sort"
+            onClick={() => {
+              setFeaturedproducts(!featuredproducts)
+            }}
+          >
+            <span>{sortname}</span>
+            <img src="./chin-img/chevron-down-black.svg" alt="" />
+          </label>
+          <ul className="chin-featuredproducts">
+            <li
+              onClick={() => {
+                setFeaturedproducts(!featuredproducts)
+                setSortname('排序方式')
+                props.AllItemsSort(props.data)
+              }}
+            >
+              全部商品
+            </li>
+            <li
+              onClick={() => {
+                setFeaturedproducts(!featuredproducts)
+                setSortname('熱銷商品')
+                props.HotItemsSort(props.data)
+              }}
+            >
+              熱銷商品
+            </li>
+            <li
+              onClick={() => {
+                setFeaturedproducts(!featuredproducts)
+                setSortname('最新商品')
+                props.NewTimeSort(props.data)
+              }}
+            >
+              最新商品
+            </li>
+            <li
+              onClick={() => {
+                setFeaturedproducts(!featuredproducts)
+                setSortname('價格:從高到低')
+                props.PriceHightToLow(props.data)
+              }}
+            >
+              價格:從高到低
+            </li>
+            <li
+              onClick={() => {
+                setFeaturedproducts(!featuredproducts)
+                setSortname('價格:從低到高')
+                props.PriceLowToHight(props.data)
+              }}
+            >
+              價格:從低到高
+            </li>
+          </ul>
         </div>
-        <div className="chin-rwd-sort-features">
-            <button className="chin-rwd-features">功能</button>
-            <button className="chin-rwd-sort">排序方式</button>
-        </div>
-        <div className="chin-comparegoods-sort">
-            <button className="chin-comparegoods">
-                <span>比較商品</span>
-                <img src="./chin-img/align-justify.svg" alt=""/>
-            </button>
-            <div className={SortClassName}>
-                <label className="chin-sort" onClick={()=>{setFeaturedproducts(!featuredproducts)}}>
-                        <span>{sortname}</span>
-                        <img src="./chin-img/chevron-down-black.svg" alt=""/>
-                </label>
-                <ul className="chin-featuredproducts">
-                    <li onClick={()=>{
-                                      setFeaturedproducts(!featuredproducts)
-                                      setSortname("排序方式")
-                                      props.AllItemsSort(props.data)}}>全部商品</li>
-                    <li onClick={()=>{
-                                      setFeaturedproducts(!featuredproducts)
-                                      setSortname("熱銷商品")
-                                      props.HotItemsSort(props.data)}}>熱銷商品</li>
-                    <li onClick={()=>{
-                                      setFeaturedproducts(!featuredproducts)
-                                      setSortname("最新商品")
-                                     props.NewTimeSort(props.data)}}>最新商品</li>
-                    <li onClick={()=>{
-                                      setFeaturedproducts(!featuredproducts)
-                                      setSortname("價格:從高到低")
-                                     props.PriceHightToLow(props.data)}}>價格:從高到低</li>
-                    <li onClick={()=>{
-                                      setFeaturedproducts(!featuredproducts)
-                                      setSortname("價格:從低到高")
-                                     props.PriceLowToHight(props.data)}}>價格:從低到高</li>
-                </ul>
-            </div>
-        </div>
+      </div>
     </div>
-    )
+  )
 }
 
 // 選擇對應的reducer
 const mapStateToProps = store => {
-    return { data: store.getItems}
-  }
-  
-  //action
-  const mapDispatchToProps = dispatch =>{
-    return bindActionCreators({
-        PriceHightToLow,PriceLowToHight,NewTimeSort,HotItemsSort,AllItemsSort
-    },dispatch)
-  }
+  return { data: store.getItems }
+}
 
-export default connect(mapStateToProps,mapDispatchToProps)(CompareProductSort)
+//action
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      PriceHightToLow,
+      PriceLowToHight,
+      NewTimeSort,
+      HotItemsSort,
+      AllItemsSort,
+    },
+    dispatch
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompareProductSort)
