b6ef48e112c09c839ef43415358c2b8271ffe7b8
NOIPA171 <58924127+NOIPA171@users.noreply.github.com>
2020-03-24 13:37:12 +0800

    更新首頁

--- src/biki/Home.js
+++ src/biki/Home.js
@@ -3,16 +3,30 @@ import { Container, Row, Col } from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import './css/all.scss'
 import './css/home.scss'
+import 'animate.css/animate.min.css'
 
 import FeaturedProducts from './components/FeaturedProducts'
 import Collection from './components/Collection'
 
 function Home(){
+
+    const handleScroll = (evt)=>{
+
+    }
+
     return(
         <>
             <section className="bk-home-slider">
-                <div className='bk-slides'>
-                    <img src="./biki-img/geronimo-giqueaux-ahPZamckL7A-unsplash.jpg" />
+                <div className='bk-mouse animated fadeIn' onScroll={handleScroll}>
+                    <div className='bk-mouse-ball animated fadeOutDown infinite'></div>
+                </div>
+                <div className='bk-slides' style={{
+                    background: `url('./biki-img/josh-nuttall-uNQ-TTg_qNY-unsplash.jpg') center center`,
+                    backgroundSize: 'cover'
+                }}>
+                    <div className='bk-slide-content'>
+                        Hello~
+                    </div>
                 </div>
             </section>
             <Container>
@@ -62,10 +76,18 @@ function Home(){
             <section className="bk-featured-products">
                 <Container>
                     <Row className='bk-featured-wrapper row-cols-xl-3 row-cols-md-2 row-cols-1'>
-                        <FeaturedProducts />
-                        <FeaturedProducts />
-                        <FeaturedProducts />
-                        <FeaturedProducts />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/SONY 重低音降噪藍牙耳罩式耳機 WH-XB900N/0.jpg`}
+                        />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/繽特力 Plantronics Voyager 6200UC 雙向降噪藍牙耳機 白色/0.jpg`}
+                        />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/GoPro-HERO8 Black全方位運動攝影機 單車騎士升級組/0.jpg`}
+                        />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/Mavic Mini 充電管家/0.jpg`}
+                        />
                         <Col>
                             <div className="bk-featured-product-item bk-box">
                                 <div className="bk-box-content">
@@ -75,10 +97,18 @@ function Home(){
                                 </div>
                             </div>
                         </Col>
-                        <FeaturedProducts />
-                        <FeaturedProducts />
-                        <FeaturedProducts />
-                        <FeaturedProducts />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/小米手環34 腕帶 替換帶 尼龍編織回環式錶帶 透氣舒適 運動智能錶帶/0.jpg`}
+                        />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/HTR 螺旋槳4726F 金銀槳 For Mavic Mini/0.jpg`}
+                        />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/SONY 運動藍牙入耳式耳機 WI-SP500/0.jpg`}
+                        />
+                        <FeaturedProducts 
+                            img={`./chin-img/images/Holy Stone HS210 迷你遙控飛機-三電版/0.jpg`}
+                        />
                     </Row>
                 </Container>
             </section>
