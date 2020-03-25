6b4983c3ca4ad3cc89984dfc316f4c6d0022b656
Biki <nightfallvs0923@gmail.com>
2020-03-25 14:59:28 +0800

    增加搜尋功能

--- db/home.js
+++ db/home.js
@@ -9,8 +9,46 @@ const db = require('./db_connect')
 // moment.locale('zh-tw'); //設置中文
 
 
-router.get('/', (req, res)=>{
-    let sql = ''
+router.get('/search', (req, res)=>{
+
+    let key = req.query.key
+    let keyarr= req.query.key.replace(/\s+/g, ' ').trim().split(' ')
+    console.log(keyarr)
+    let selectArr = [
+        '`itemId`', 
+        '`name`', 
+        '`itemName`', 
+        '`itemImg`', 
+        '`itemDescription`', 
+        '`itemPrice`', 
+        '`itemCategoryId`'
+    ]
+
+    let likeStr = ``;
+
+    keyarr.forEach((elm, idx)=>{
+        selectArr.forEach((e, i)=>{
+            if(idx === keyarr.length -1 && i === selectArr.length -1){
+                likeStr += `${e} LIKE '%${elm}' OR ${e} LIKE '${elm}%' OR ${e} LIKE '%${elm}%'`
+            }else{
+                likeStr += `${e} LIKE '%${elm}' OR ${e} LIKE '${elm}%' OR ${e} LIKE '%${elm}%' OR `
+            }
+        })
+    })
+
+    let sql = `SELECT ${selectArr.join(' ,')} FROM \`items\` WHERE ${likeStr}`;
+
+    //console.log(sql)
+
+    db.queryAsync(sql)
+    .then(r=>{
+        res.json(r)
+        // console.log(r)
+    })
+    .catch(err=>{
+        res.json(err)
+        console.log(err)
+    })
 })
 
 module.exports = router;
