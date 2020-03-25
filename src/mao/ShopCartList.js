b3e7ca11ba8bc4b61aeb4148a7c6a124f541a191
Chang <morweng0113@gmail.com>
2020-03-25 14:41:43 +0800

    new

--- src/mao/ShopCartList.js
+++ src/mao/ShopCartList.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './css/mao.scss'
 import './css/ShopCartList.scss'
-// import '../css/variables.scss'
 import MaoCartShopTotal from './component/MaoCartShopTotal'
 import { withRouter, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
@@ -13,35 +12,20 @@ import {
   DelCartItem,
   CalShopCart,
   Handle_AddMyFavorite,
-  ControlDataOne,
+  ControlDataOne
 } from './actions/ShopCartAction'
+import {commidtyRANDItemId} from '../chin/actions/itemsActions'
 import { productList } from './ProductList'
 import ProductSlide from './ProductSlide'
-import { FaRegTrashAlt} from "react-icons/fa"
-import { FiHeart} from 'react-icons/fi';
+import RelatedHistory from '../chin/components/RelatedHistory'
+import { FaRegTrashAlt } from 'react-icons/fa'
+import { FiHeart } from 'react-icons/fi'
 import MaoAD from './component/MaoAD'
 function ShopCartList(props) {
+  console.log('看我這邊~~ == ',props)
   const [loaded, setLoaded] = useState(false)
   const [forCart, setForCart] = useState(false)
-  // 從產品ID轉換成產品名稱
-  function checkProduct(val) {
-    productList.map((v, i) => {
-      if (val == v.itemId) {
-        val = v.pName
-      }
-    })
-    return val
-  }
-
-  // 從ID去獲取產品的價格
-  function checkProductPrice(val) {
-    productList.map((v, i) => {
-      if (val == v.itemId) {
-        val = v.itemPrice
-      }
-    })
-    return val
-  }
+  const [newItem,setNewItem] =useState(false)
   // 必打
   async function getData() {
     let Ctrl = await props.CtrlData
@@ -49,17 +33,15 @@ function ShopCartList(props) {
     await props.getShopCart(Ctrl)
   }
   useEffect(() => {
-    checkProduct()
     getData()
     setLoaded(true)
     setForCart(false)
-    console.log(props)
+    props.commidtyRANDItemId()
   }, [])
 
-
-  useEffect(()=>{
-    console.log('我是生命週期裡的東西 = ',props.AddItem)
-  },[props.AddItem,<ProductSlide/>])
+  // useEffect(() => {
+  //   console.log('我是生命週期裡的東西 = ', props.AddItem)
+  // }, [props.AddItem])
 
   let RealCart = [] //統整checkBox的品項，然後最後送至資料庫
 
@@ -82,11 +64,15 @@ function ShopCartList(props) {
   const dataList = props.AddItem.map((v, i) => {
     return (
       <li key={v} className="Mao-shopcart-check-item">
-        <img src="https://fakeimg.pl/100/" alt="" />
+        <img
+          src={`./chin-img/images/${v.itemName}/${v.itemImg}`}
+          alt=""
+          style={{ width: '100px', height: '100px' }}
+        />
         <div className="d-flex flex-column justify-content-between Mao-shopcart-check-item-info">
           <p>{v.itemName}</p>
           <div className="d-flex justify-content-between">
-            <p style={{ width: '25%' }}>${v.itemPrice}</p>
+            <p style={{ width: '25%' }}>{v.itemPrice}</p>
             <div className="d-flex justify-content-between align-items-center Mao-shopcart-check-item-count">
               <button
                 className="Mao-btn-amount"
@@ -98,11 +84,7 @@ function ShopCartList(props) {
               >
                 -
               </button>
-              <input
-                placeholder=""
-                value={v.count}
-                type="text"
-              />
+              <input placeholder="" value={v.count} type="text" />
               <button
                 className="Mao-btn-amount"
                 onClick={() => {
@@ -124,7 +106,14 @@ function ShopCartList(props) {
               props.DelCartItem(i, props.AddItem)
             }}
           >
-            <FaRegTrashAlt style={{width:'25px',height:'25px',marginRight:'15px',marginLeft:'18px'}}/>
+            <FaRegTrashAlt
+              style={{
+                width: '25px',
+                height: '25px',
+                marginRight: '15px',
+                marginLeft: '18px',
+              }}
+            />
             <span>刪除</span>
             <div className="Mao-btn-show"></div>
             <div className="Mao-btn-showL"></div>
@@ -137,7 +126,14 @@ function ShopCartList(props) {
               props.Handle_AddMyFavorite('true', v, props.MyFavorite)
             }}
           >
-            <FiHeart  style={{width:'25px',height:'25px',marginRight:'15px',marginLeft:'18px'}}/>
+            <FiHeart
+              style={{
+                width: '25px',
+                height: '25px',
+                marginRight: '15px',
+                marginLeft: '18px',
+              }}
+            />
             <span>下次購買</span>
             <div className="Mao-btn-show"></div>
             <div className="Mao-btn-showL"></div>
@@ -148,26 +144,29 @@ function ShopCartList(props) {
   })
   // 如果沒有購物車內沒有品項顯示的畫面
   const CartNoItem = (
-   
     <div className="p-3 text-center Mao-CartNoItem-shoplist">
       <h3>趕快去尋找最愛的商品吧！</h3>
-      <Link to="/">
-      </Link>
+      <Link to="/"></Link>
     </div>
   )
   const CartNoItemTotal = <div className="Mao-Total-Box-none"></div>
 
   return (
     <>
-      <MaoAD/>
+      <MaoAD />
       <div className="d-flex my-3" style={{ maxWidth: '1300px' }}>
-        <ul className='Mao-shopcart-check-item-ul'>
+        <ul className="Mao-shopcart-check-item-ul">
           {props.AddItem.length > 0 ? dataList : CartNoItem}
         </ul>
         {props.AddItem.length > 0 ? <MaoCartShopTotal /> : CartNoItemTotal}
       </div>
       {displayRealCart}
-      <ProductSlide />
+      <ProductSlide 
+      getdata={newItem} //hook
+      sendData={items=>{ //func
+        setNewItem(items)
+      }}
+      />
     </>
   )
 }
@@ -178,9 +177,10 @@ const mapStateToProps = store => {
     //購物車內容
     // data: store.getShop,
     AddItem: store.AddItem,
-    // Cart: store.displayShopCart,
+    getRANDitemid:store.getRANDitemid,
     MyFavorite: store.MyFavorite,
     CtrlData: store.ControlDataState,
+    getItems:store.getItems
   }
 }
 
@@ -194,7 +194,7 @@ const mapDispatchToProps = dispatch => {
       DelCartItem,
       CalShopCart,
       Handle_AddMyFavorite,
-      ControlDataOne,
+      ControlDataOne,commidtyRANDItemId
     },
     dispatch
   )
