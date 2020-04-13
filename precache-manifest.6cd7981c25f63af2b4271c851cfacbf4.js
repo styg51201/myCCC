1a14c483fda256b898e5586afe7063f29890a023
StaceyZhan <styg51201@gmail.com>
2020-04-13 14:51:48 +0800

    Updates

---
+++ precache-manifest.6cd7981c25f63af2b4271c851cfacbf4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b7fce5cf1a49a0b4ef2a3a737b8d24",
+    "url": "/myCCC/index.html"
+  },
+  {
+    "revision": "aabc7b3e80477e815be7",
+    "url": "/myCCC/static/css/2.e1bd35af.chunk.css"
+  },
+  {
+    "revision": "d456d0bcf3e57dcfaeaf",
+    "url": "/myCCC/static/css/main.0ec9c425.chunk.css"
+  },
+  {
+    "revision": "aabc7b3e80477e815be7",
+    "url": "/myCCC/static/js/2.3501ada2.chunk.js"
+  },
+  {
+    "revision": "e857a3658730f1cc609214c3f3735d15",
+    "url": "/myCCC/static/js/2.3501ada2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d456d0bcf3e57dcfaeaf",
+    "url": "/myCCC/static/js/main.90fadbfd.chunk.js"
+  },
+  {
+    "revision": "6692984a484cc7b122a0",
+    "url": "/myCCC/static/js/runtime-main.49d502c4.js"
+  },
+  {
+    "revision": "e4e2691c96d0bc594beeb6ba555da356",
+    "url": "/myCCC/static/media/32.e4e2691c.png"
+  },
+  {
+    "revision": "b476845178aaa3f8014956fd6a773e60",
+    "url": "/myCCC/static/media/33.b4768451.png"
+  },
+  {
+    "revision": "e36f09e74a343ba137d33b1b38551dd3",
+    "url": "/myCCC/static/media/34.e36f09e7.png"
+  },
+  {
+    "revision": "700b0130738a3613e4b44a06b7e341a9",
+    "url": "/myCCC/static/media/35.700b0130.png"
+  },
+  {
+    "revision": "b476845178aaa3f8014956fd6a773e60",
+    "url": "/myCCC/static/media/36.b4768451.png"
+  },
+  {
+    "revision": "a7290bedc83acf1da0037299c8686c70",
+    "url": "/myCCC/static/media/37.a7290bed.png"
+  },
+  {
+    "revision": "c537661dc42fa7b8277745f97fb8a72a",
+    "url": "/myCCC/static/media/38.c537661d.png"
+  },
+  {
+    "revision": "e36f09e74a343ba137d33b1b38551dd3",
+    "url": "/myCCC/static/media/39.e36f09e7.png"
+  },
+  {
+    "revision": "a7290bedc83acf1da0037299c8686c70",
+    "url": "/myCCC/static/media/40.a7290bed.png"
+  },
+  {
+    "revision": "700b0130738a3613e4b44a06b7e341a9",
+    "url": "/myCCC/static/media/41.700b0130.png"
+  },
+  {
+    "revision": "c537661dc42fa7b8277745f97fb8a72a",
+    "url": "/myCCC/static/media/42.c537661d.png"
+  },
+  {
+    "revision": "583a7fbccefe6c1cd9c3b5135d732efe",
+    "url": "/myCCC/static/media/logo.583a7fbc.svg"
+  }
+]);
